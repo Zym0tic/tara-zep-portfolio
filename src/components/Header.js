051b30a61f69e2e1750c919d9b0a4ebb1715a46d
zym0tic <lukerozon@gmail.com>
2022-12-01 14:12:14 -0500

    add imports to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,12 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+// import data
+import { headerData } from '../data';
+//import components
+import Nav from './Nav';
+import NavMobile from './NavMobile';
+import Socials from './Socials';
+//import icons
+
 
 const Header = () => {
   return <div>Header</div>;
