90ff604c73d8eda8cd2190187e58dd6dad51d2df
zym0tic <lukerozon@gmail.com>
2022-12-03 14:17:31 -0500

    add more styles to about section

--- src/components/About.js
+++ src/components/About.js
@@ -15,18 +15,17 @@ const About = () => {
         <div className="flex flex-col lg:flex-row gap-x-[70px] items-center">
           {/* numbers */}
           <div className="flex flex-col lg:flex-row flex-1">
-            <div className="text-[360px] xl:text-[720px] leading-none font-tertiary lg:tracking-[0.055em] bg-about bg-no-repeat bg-right bg-clip-text text-transparent">TZ</div>
+            <div className="text-[360px] xl:text-[720px] leading-none font-tertiary lg:-tracking-[0.055em] bg-about bg-no-repeat bg-right bg-clip-text text-transparent">TZ</div>
           </div>
           {/* text */}
           <div className="flex-1 h-full xl:mt-48">
             <h2 className="h2">{title}</h2>
-            <div className="flex flex-col items-end bg-pink-200">
+            <div className="flex flex-col items-end">
               <div className="max-w-[530px] text-grey">
-                <p>{subtitle1}</p>
-                <p>{subtitle2}</p>
-                <button>
-                  {btnText} <div>{btnIcon}</div>
-                  {""}
+                <p className='mb-6'>{subtitle1}</p>
+                <p className='mb-9'>{subtitle2}</p>
+                <button className="btn btn-lg btn-link">
+                  {btnText} <div className="text-xl">{btnIcon}</div>
                 </button>
               </div>
             </div>
