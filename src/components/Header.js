98b982571fc664511b7957e44bf2f28175e8b7cb
zym0tic <lukerozon@gmail.com>
2022-12-02 21:14:22 -0500

    stylized header, nav, Socials

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,7 +26,7 @@ const Header = () => {
     <header
       className={`${
         isActive ? "h-[100px] lg:h-[110px] shadow-lg" : "h-[120px] lg:h-{150px}"
-      } fixed left-0 right-0 z-10 max-w-[1920px] w-full mx-auto transition-all duration-300`}
+      } fixed bg-white left-0 right-0 z-10 max-w-[1920px] w-full mx-auto transition-all duration-300`}
     >
       <div className="flex justify-between items-center h-full pl-[50px] pr-[60px]">
         {/* logo */}
@@ -54,6 +54,10 @@ const Header = () => {
         >
           <NavMobile />
         </div>
+  {/* social icons hidden show on desktop */}
+  <div className="hidden xl:flex">
+    <Socials />
+  </div>
       </div>
     </header>
   );
