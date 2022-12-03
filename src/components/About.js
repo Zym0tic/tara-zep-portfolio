dbd1c87f75eda5bf4e8554ff91eb1bb289ced038
zym0tic <lukerozon@gmail.com>
2022-12-03 13:25:04 -0500

    add content to about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React from "react";
 //import about data
-import { aboutData } from '../data';
+import { aboutData } from "../data";
 // import motion
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 //import variants
-import { fadeIn } from '../variants';
+import { fadeIn } from "../variants";
 
 const About = () => {
   //destructure about data
   const { title, subtitle1, subtitle2, btnText, btnIcon } = aboutData;
-  return <div>About</div>;
+  return (
+    <section className="bg-pink-200">
+      <div className="container mx-auto">
+        <div>
+          {/* numbers */}
+          <div>
+            <div>01</div>
+          </div>
+          {/* text */}
+          <div>
+            <h2>{title}</h2>
+            <div>
+              <div>
+                <p>{subtitle1}</p>
+                <p>{subtitle2}</p>
+                <button>
+                  {btnText} <div>{btnIcon}</div>
+                  {""}
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default About;
