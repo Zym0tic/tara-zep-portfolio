3e11b382915efe4eccbcde6ca49097558aec0e5e
zym0tic <lukerozon@gmail.com>
2022-12-03 12:48:57 -0500

    add imports to about section

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,14 @@
 import React from 'react';
+//import about data
+import { aboutData } from '../data';
+// import motion
+import { motion } from 'framer-motion';
+//import variants
+import { fadeIn } from '../variants';
 
 const About = () => {
+  //destructure about data
+  const { title, subtitle1, subtitle2, btnText, btnIcon } = aboutData;
   return <div>About</div>;
 };
 
