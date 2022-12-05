f70ca8915e4736ac1aa85b159fe7bd4f9bd1c29a
zym0tic <lukerozon@gmail.com>
2022-12-04 22:02:45 -0500

    add imports to GallerySection

--- src/components/GallerySection.js
+++ src/components/GallerySection.js
@@ -1,4 +1,17 @@
 import React from 'react';
+//import data
+import { galleryData } from '../data';
+// import photo album nad lightbox
+import { PhotoAlbum } from 'react-photo-album';
+import Lightbox from 'yet-another-react-lightbox';
+//lightbox css
+import 'yet-another-react-lightbox/styles.css'
+//import motion
+import { motion } from 'framer-motion';
+//import fadein
+import { fadeIn } from '../variants';
+
+
 
 const GallerySection = () => {
   return <div>GallerySection</div>;
