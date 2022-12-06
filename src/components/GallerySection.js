514a4a300de98e0927ce08188558e4368378b9ae
zym0tic <lukerozon@gmail.com>
2022-12-05 20:47:56 -0500

    add styles and motion to photo gallery

--- src/components/GallerySection.js
+++ src/components/GallerySection.js
@@ -1,18 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 //import data
-import { galleryData } from '../data';
+import { galleryData } from "../data";
 // import photo album nad lightbox
-import { PhotoAlbum } from 'react-photo-album';
-import Lightbox from 'yet-another-react-lightbox';
+import { PhotoAlbum } from "react-photo-album";
+import Lightbox from "yet-another-react-lightbox";
 //lightbox css
-import 'yet-another-react-lightbox/styles.css'
+import "yet-another-react-lightbox/styles.css";
 //import motion
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 //import fadein
-import { fadeIn } from '../variants';
+import { fadeIn } from "../variants";
 
 //slides
-const slides = galleryData.images.map(({original, width, height}) => ({
+const slides = galleryData.images.map(({ original, width, height }) => ({
   src: original,
   width,
   height,
@@ -24,13 +24,55 @@ const GallerySection = () => {
   //destructure gallery data
   const { title, btnText, btnIcon, images } = galleryData;
 
-  return <section className='bg-pink-200 section relative mt-[40px] lg:mt-0'>
-    <div className='container mx-auto'>
-      <h2>{title}</h2>
-    </div>
-  {/* photo album */}
-  <PhotoAlbum layout='rows' photos={images}/> 
-    </section>;
+  return (
+    <section className="bg-[#f9f9f9] section relative mt-[40px] lg:mt-0">
+      <div className="container mx-auto">
+        <motion.h2
+          variants={fadeIn("up")}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.6 }}
+          className="h2 wax-w-[370px] lg:mb-20"
+        >
+          {title}
+        </motion.h2>
+      </div>
+      {/* photo album */}
+      <motion.div
+        variants={fadeIn("up")}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, amount: 0.2 }}
+        className="mb-8 lg:mb-20"
+      >
+        <PhotoAlbum
+          onClick={(event, photo, index) => setIndex(index)}
+          layout="rows"
+          photos={images}
+        />
+        <Lightbox
+          slides={slides}
+          styles={{ container: { backgroundColor: "rgba(0,0,0,.9)" } }}
+          open={index >= 0}
+          index={index}
+          close={() => setIndex(-1)}
+        />
+      </motion.div>
+      {/* btn */}
+      <motion.div
+        variants={fadeIn("up")}
+        initial="hidden"
+        whileInView={"show"}
+        viewport={{ once: false, amount: 0.2 }}
+        className="flex justify-center"
+      >
+        <button className="btn btn-lg btn-dark">
+          {" "}
+          {btnText} <div className="text-xl"> {btnIcon} </div>{" "}
+        </button>
+      </motion.div>
+    </section>
+  );
 };
 
 export default GallerySection;
