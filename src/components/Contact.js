d2169c283574fe4302ef1e73a40ee41c3c3c317a
zym0tic <lukerozon@gmail.com>
2022-12-07 11:57:58 -0500

    import contact data, motion, fade in to contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,7 +1,24 @@
 import React from 'react';
+//import contact data
+import { contactData } from '../data';
+//import motion
+import { motion } from 'framer-motion';
+//import fade in
+import { fadeIn } from '../variants';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  //destructure contact data
+  const { title, info, form } = contactData;
+  return <section className='bg-pink-200 section'>
+    <div className='container mx-auto'>
+      <div>
+        {/* text */}
+        <div>
+          {/* title */}
+        </div>
+      </div>
+    </div>
+    </section>;
 };
 
 export default Contact;
