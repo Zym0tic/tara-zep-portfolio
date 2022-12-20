99112f944c1a4b65344853fb428ff60ac3989155
zym0tic <lukerozon@gmail.com>
2022-12-20 18:58:35 -0500

    finished press section

--- src/components/press.js
+++ src/components/press.js
@@ -1,15 +1,22 @@
 import React from "react";
 //import press data
 import { pressData } from "../data";
+//import motion
+import { motion } from "framer-motion";
+//import fade in
+import { fadeIn } from "../variants";
 
 const Press = () => {
   // destructure press data
   const { title, items } = pressData;
   return (
     <section className="section bg-dark bg-interview bg-no-repeat bg-cover bg-center lg:h-[812px]" name="press">
-      <div className="container mx-auto">
+      <motion.div variants={fadeIn("left")}
+            initial="hidden"
+            whileInView={"show"}
+            viewport={{ once: false, amount: 0.4 }} className="container mx-auto text-white">
         {/* title */}
-        <h2 className="h2">{title}</h2>
+        <h2 className="h2 text-white">{title}</h2>
         {/* links */}
         <div>
           <ul>
@@ -17,15 +24,15 @@ const Press = () => {
               //destructure item
               const { href, subtitle } = item;
               return (
-                <div key={index}>
+                <div key={index} className='mb-6 font-bold leading-[187%] tracking-[0.02em]'>
                   {/* link */}
-                  <a href={href}>{subtitle}</a>
+                  <a href={href} target="_blank" className="link hover:border-b-2 hover:border-white transition duration-300 cursor-pointer">{subtitle}</a>
                 </div>
               )
             })}
           </ul>
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 };
