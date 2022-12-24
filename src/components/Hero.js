48fd2a2d266862c308a3914ff4d6a0e7e37de4fd
zym0tic <lukerozon@gmail.com>
2022-12-24 16:50:04 -0500

    fix shadow and fomt weight on hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -34,7 +34,7 @@ const Hero = () => {
           </motion.h1>
           <motion.p
             variants={fadeIn("down")}
-            className="mb-8 lg:mb-16 max-w-lg leading-relaxed"
+            className="hero-p mb-8 lg:mb-16 max-w-lg leading-relaxed"
           >
             {subtitle}
           </motion.p>
