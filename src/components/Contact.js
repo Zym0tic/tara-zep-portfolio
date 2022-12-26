5c9358676e2af01febe2b0ddf305e7628b0aee10
zym0tic <lukerozon@gmail.com>
2022-12-26 18:09:54 -0500

    hooked up email js to my email, awaiting clients info

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,14 +1,47 @@
-import React from "react";
+import { React, useState } from "react";
 //import contact data
 import { contactData } from "../data";
 //import motion
 import { motion } from "framer-motion";
 //import fade in
 import { fadeIn } from "../variants";
+//import emailjs
+import { send } from "emailjs-com";
 
 const Contact = () => {
   //destructure contact data
   const { title, info, form } = contactData;
+  //form submission
+  const [toSend, setToSend] = useState({
+    from_name: '',
+    to_name: 'Luke',
+    message: '',
+    reply_to: '',
+  });
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    /* --- METHOD TO SEND THE MAIL --- */
+    send(
+      'service_pzxljez',
+      'template_mqp0mfm',
+      toSend,
+      '0I25YihcNkDWx1iIO'
+    )
+      .then((response) => {
+        console.log('SUCCESS!', response.status, response.text);
+      })
+      .catch((err) => {
+        console.log('FAILED...', err);
+      });
+  };
+
+  const handleChange = (e) => {
+    setToSend({ ...toSend, [e.target.name]: e.target.value });
+  };
+
+
+
   return (
     <section className="section" name="contact">
       <div className="container mx-auto">
@@ -57,21 +90,30 @@ const Contact = () => {
             viewport={{ once: false, amount: 0.4 }}
             className="flex-1 xl:pl-[40px] flex justify-center items-center"
           >
-            <form className="flex flex-col gap-y-10 w-full">
+            <form className="flex flex-col gap-y-10 w-full" onSubmit={onSubmit}>
               <input
                 className="border-b border-dark placeholder:text-[#555] italic trackingg-[0.06em] outline-none pb-4"
                 placeholder={form.name}
                 type="text"
+                name="from_name"
+                value={toSend.from_name}
+                onChange={handleChange}
               />
               <input
                 className="border-b border-dark placeholder:text-[#555] italic trackingg-[0.06em] outline-none pb-4"
                 placeholder={form.email}
                 type="text"
+                name="reply_to"
+                value={toSend.reply_to}
+                onChange={handleChange}
               />
               <input
                 className="border-b border-dark placeholder:text-[#555] italic trackingg-[0.06em] outline-none pb-4"
                 placeholder={form.message}
                 type="text"
+                name="message"
+                value={toSend.message}
+                onChange={handleChange}
               />
               {/* button */}
               <button className="btn btn-sm btn-dark self-start">
